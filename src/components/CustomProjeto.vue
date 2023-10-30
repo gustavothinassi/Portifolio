421f3478e2f19388650d40454b363b3d9abdfdcf
gustavothinassi <gustavo.basilio018@gmail.com>
2023-10-30 10:09:57 -0300

    refactor:custom project

--- src/components/CustomProjeto.vue
+++ src/components/CustomProjeto.vue
@@ -67,7 +67,7 @@
           </p>
         </div>
         <div class="px-6 pt-4 pb-6">
-          <a href="https://promobarato.vercel.app/"
+          <a href="#"
             class="bg-gray-200 text-sm uppercase rounded-xl mr-4 text-black font-bold py-2 px-4 inline-flex items-center hover:opacity-75">
             <img class="w-5 h-5 mr-2" src="../assets/images/eye.png" />
             <span>Demo</span>
